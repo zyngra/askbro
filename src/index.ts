832f095996f04cd3f47e878c55a85e5b8da8a0b8
Leandro Romanzini <leandro.romanzini@gmail.com>
2023-03-17 00:41:06 -0300

    changes typescript indent size

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,9 @@ const app: Express = express();
 const port: Number = 3000;
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello world from Typescript!");
+    res.send("Hello world from Typescript!");
 });
 
 app.listen(port, () => {
-  console.log(`Helow! Listening on port ${port}`);
+    console.log(`Helow! Listening on port ${port}`);
 });
